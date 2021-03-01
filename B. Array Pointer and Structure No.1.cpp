7671a6d682e3edad8bc828982fd6af6265effe00
Filzahisana <72488481+Filzahisana@users.noreply.github.com>
2021-03-02 00:02:30 +0700

    Add files via upload

---
+++ B. Array Pointer and Structure No.1.cpp
@@ -0,0 +1,20 @@
+/*Tugas2 Prak. Struktur Data*/
+/*B. Array Pointer and Structure */
+/*Filza Hisana*/
+/*NIM: 20051397018*/
+
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+	char kalimat[100];
+	cout<<"Masukan Kalimat :"; cin.getline (kalimat , sizeof (kalimat) );
+	int x = strlen(kalimat);
+	for(int i=x-1;i>=0;i--)
+	{
+		cout<<kalimat[i];
+ 	}
+}
